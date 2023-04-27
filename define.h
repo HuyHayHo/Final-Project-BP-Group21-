c77ff17dc550aa8e261b48cba541e794a219e839
HuyHayHo <huyhayphet123@gmail.com>
2023-04-27 19:49:34 +0700

    Final BP Group21

---
+++ define.h
@@ -0,0 +1,23 @@
+#define TRANHUY 20
+#define TRONGNGHIA 20
+#define TRUONGHUY 20
+#define KHACCONG 20
+#define MINHDUC 20
+#define HOANGHA 20
+#define KIEN 20
+#define VANMANH 20
+#define PHUCTHINH 20
+#define THANHNAM 20
+struct Student {
+    char studentID[20];
+    char lastName[20];
+    char firstName[20];
+    char birthdate[11];
+    char bir[11];
+    char thd[11];
+    char ate[11];
+    float algebraGrade;
+    float calculusGrade;
+    float programmingGrade;
+    float gpa;
+};
