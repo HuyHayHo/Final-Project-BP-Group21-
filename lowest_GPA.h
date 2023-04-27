06cfad5a2317312296703982a91cca8c43373f2b
HuyHayHo <92163299+HuyHayHo@users.noreply.github.com>
2023-04-27 20:13:59 +0700

    Delete lowest_GPA.h

--- lowest_GPA.h
+++
@@ -1,9 +0,0 @@
-struct Student findLowestGPA(struct Student students[], int numOfStudents) {
-    struct Student lowestGPAStudent = students[0];
-    for (int i = 1; i < numOfStudents; i++) {
-        if (students[i].gpa < lowestGPAStudent.gpa) {
-            lowestGPAStudent = students[i];
-        }
-    }
-    return lowestGPAStudent;
-}
