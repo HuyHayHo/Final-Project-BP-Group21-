8c75f5b5813271befd17b54268f9fd38d168244b
HuyHayHo <92163299+HuyHayHo@users.noreply.github.com>
2023-04-27 20:13:51 +0700

    Delete highest_BP.h

--- highest_BP.h
+++
@@ -1,9 +0,0 @@
-struct Student findHighestBP(struct Student students[], int numOfStudents) {
-    struct Student highestBPStudent = students[0];
-    for (int i = 1; i < numOfStudents; i++) {
-        if (students[i].programmingGrade > highestBPStudent.programmingGrade) {
-            highestBPStudent = students[i];
-        }
-    }
-    return highestBPStudent;
-}
